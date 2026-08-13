af2b864f8ed0e3b4567d1f148111b75ed5d4eca5
idkdontaskm3 <idkdontaskm3@proton.me>
2026-08-13 15:47:11 -0700

    mediaharbor: 2.1.0 -> 2.2.0

---
+++ pkgs/by-name/me/mediaharbor/package.nix
@@ -0,0 +1,107 @@
+{
+  atk,
+  cairo,
+  cargo-tauri,
+  dbus,
+  fetchFromGitHub,
+  fetchNpmDeps,
+  ffmpeg,
+  gdk-pixbuf,
+  glib,
+  gst_all_1,
+  gtk3,
+  harfbuzz,
+  lib,
+  libgit2,
+  libsoup_3,
+  nodejs,
+  npmHooks,
+  openssl,
+  pango,
+  pkg-config,
+  rustPlatform,
+  typescript,
+  webkitgtk_4_1,
+  wrapGAppsHook4,
+  zlib,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "mediaharbor";
+  version = "2.2.0";
+
+  src = fetchFromGitHub {
+    owner = "MediaHarbor";
+    repo = "mediaharbor";
+    tag = "v${finalAttrs.version}";
+    hash = "";
+  };
+
+  __structuredAttrs = true;
+  env.LIBGIT2_NO_VENDOR = true;
+
+  cargoHash = "";
+
+  cargoBuildFlags = [
+    "-p"
+    "mediaharbor"
+  ];
+
+  npmRebuildFlags = [ "--ignore-scripts" ];
+
+  npmDeps = fetchNpmDeps {
+    src = finalAttrs.src;
+    hash = "sha256-66Z42cZQ/bW5Dn0Wfz6MWbIgrI9NF3XsNj4j5KhIzBY=";
+  };
+
+  nativeBuildInputs = [
+    cargo-tauri.hook
+    nodejs
+    npmHooks.npmConfigHook
+    pkg-config
+    typescript
+    wrapGAppsHook4
+  ];
+
+  buildInputs = [
+    atk
+    cairo
+    dbus
+    ffmpeg
+    gdk-pixbuf
+    glib
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gtk3
+    harfbuzz
+    libgit2
+    libsoup_3
+    nodejs
+    openssl
+    pango
+    webkitgtk_4_1
+    zlib
+  ];
+
+  preBuild = ''
+    npm run build:react
+  '';
+
+  dontUseCargoInstall = true;
+
+  installPhase = ''
+    mkdir -p $out/bin $out/share
+    install -Dm755 target/x86_64-unknown-linux-gnu/release/mediaharbor $out/bin/
+    cp -r target/x86_64-unknown-linux-gnu/release/bundle/deb/MediaHarbor_*/data/usr/share/* $out/share/ || true
+  '';
+
+  meta = {
+    description = "Cross-platform Media Ripping and Browsing GUI";
+    homepage = "https://mediaharbor.org/";
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ idkdontaskm3 ];
+    mainProgram = "mediaharbor";
+  };
+})
